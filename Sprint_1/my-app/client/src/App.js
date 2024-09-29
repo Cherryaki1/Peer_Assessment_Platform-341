3b0d37be1fc93a176e380c87c749c6dc102ebe4f
notxplosion <greglajoie55@gmail.com>
2024-09-29 19:33:12 -0400

    quick fix

--- Sprint_1/my-app/client/src/App.js
+++ Sprint_1/my-app/client/src/App.js
@@ -1,11 +1,10 @@
 // src/app/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import UserDashboard from './components/UserDashboard';
-import AdminDashboard from './components/AdminDashboard';
+import Login from './app/Login';
+import UserDashboard from './app/UserDashboard';
+import AdminDashboard from './app/AdminDashboard';
 import './index.css';
-
 
 
 const App = () => {
--- Sprint_1/my-app/client/src/app/Login.jsx
+++ Sprint_1/my-app/client/src/app/Login.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+
 const Login = () => {
     const [ID, setID] = useState('');
     const [password, setPassword] = useState('');

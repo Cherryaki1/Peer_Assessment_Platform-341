3c88a7a3fd5a8b64c80cdc3cfbf0943c58e4607d
Cherryaki <Botao5339@gmail.com>
2024-09-29 16:20:25 -0400

    tailwind.config.js
    implemented craco.config.js and modified tailwind.config.js to match craco

--- Sprint_1/my-app/client/tailwind.config.js
+++ Sprint_1/my-app/client/tailwind.config.js
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      backgroundImage: theme => ({
+        'hero-pattern': "url('/src/images/hero-pattern.svg')",
+        'footer-texture': "url('/src/images/footer-texture.svg')",
+      })
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],

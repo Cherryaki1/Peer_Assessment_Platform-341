2a42044a55a02ef17414d62d735dd2f85e332f80
fuji2828 <fungjeremy10@gmail.com>
2024-10-18 12:25:22 -0400

    it's not working yet, just pushing so tameera can try

--- my-app/server/models/instructorModel.js
+++ my-app/server/models/instructorModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const instructorSchema = new mongoose.Schema({
     FirstName: { type: String, required: true },
     LastName: { type: String, required: true },
-    ID: { type: Number, unique: true, required: true },
+    instructorID: { type: Number, unique: true, required: true },
     Email: { type: String, unique: true, required: true },
     Username: { type: String, required: true },
     Department: { type: String, required: true },

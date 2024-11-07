4e652fc56879812ff059a667193bd392e60b2b40
Abdulah Ghulam Ali <abdulah.ghulamali@gmail.com>
2024-11-07 12:35:44 -0500

    Revert

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -875,6 +875,7 @@ app.get('/hasRatedInstructor', async (req, res) => {
     }
 });
 
+
 app.get('/index', (req, res) => {
     if (req.isAuthenticated()) {
         res.json({ user: req.user, message: '' });

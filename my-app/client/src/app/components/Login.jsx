756f13ff8d4044c54a5a36a19288fed9a1362430
James <jamessheldona@gmail.com>
2024-11-16 12:31:03 -0500

    fix

--- my-app/client/src/app/components/Login.jsx
+++ my-app/client/src/app/components/Login.jsx
@@ -33,35 +33,101 @@ const Login = () => {
                 <source src={BackgroundVideo} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
-            <div className="bg-slate-800 border border-slate-600 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-60 relative">
-                <h1 className="text-4xl text-white font-bold text-center mb-1">Rice++</h1>
-                <h2 className="text-3xl text-white font-bold text-center mb-6">Peer Assessment</h2>
+            <div className="bg-slate-800 
+            border 
+            border-slate-600 
+            rounded-md 
+            p-8 
+            shadow-lg 
+            backdrop-filter 
+            backdrop-blur-sm 
+            bg-opacity-60 
+            relative">
+                <h1 className="text-4xl 
+                text-white 
+                font-bold 
+                text-center 
+                mb-1">Rice++</h1>
+                <h2 className="text-3xl 
+                text-white 
+                font-bold 
+                text-center 
+                mb-6">Peer Assessment</h2>
                 <form onSubmit={handleSubmit}>
                     <div className="relative my-4">
                         <input
                             type="text"
                             id="ID"
-                            className="block w-72 py-2 px-0 text-sm text-white bg-transparent border-b-2 border-gray-300 focus:outline-none focus:border-emerald-600 peer"
+                            className="block 
+                            w-72 
+                            py-2 
+                            px-0 
+                            text-sm 
+                            text-white 
+                            bg-transparent 
+                            border-b-2 
+                            border-gray-300 
+                            focus:outline-none 
+                            focus:border-emerald-600 
+                            peer"
                             value={ID}
                             onChange={(e) => setID(e.target.value)}
                             required
                         />
-                        <label htmlFor="ID" className="absolute text-sm text-white duration-300 transform scale-75 top-3 -z-10 peer-focus:scale-75 peer-focus:-translate-y-6">ID</label>
+                        <label htmlFor="ID" className="absolute 
+                        text-sm 
+                        text-white 
+                        duration-300 
+                        transform 
+                        scale-75 
+                        top-3 
+                        -z-10 
+                        peer-focus:scale-75 
+                        peer-focus:-translate-y-6">ID</label>
                         <BiUser className="absolute top-4 right-4" />
                     </div>
                     <div className="relative my-4">
                         <input
                             type="password"
                             id="password"
-                            className="block w-72 py-2 px-0 text-sm text-white bg-transparent border-b-2 border-gray-300 focus:outline-none focus:border-emerald-600 peer"
+                            className="block 
+                            w-72 
+                            py-2 
+                            px-0 
+                            text-sm 
+                            text-white 
+                            bg-transparent 
+                            border-b-2 
+                            border-gray-300 
+                            focus:outline-none 
+                            focus:border-emerald-600 
+                            peer"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             required
                         />
-                        <label htmlFor="password" className="absolute text-sm text-white duration-300 transform scale-75 top-3 -z-10 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+                        <label htmlFor="password" className="absolute 
+                        text-sm 
+                        text-white 
+                        duration-300 
+                        transform 
+                        scale-75 
+                        top-3 
+                        -z-10 
+                        peer-focus:scale-75 
+                        peer-focus:-translate-y-6">Password</label>
                         <AiOutlineUnlock className="absolute top-4 right-4" />
                     </div>
-                    <button className="w-full mt-6 rounded-full bg-white text-emerald-800 hover:bg-emerald-600 hover:text-white py-2 transition-colors duration-300" type="submit">Login</button>
+                    <button className="w-full 
+                    mt-6 
+                    rounded-full 
+                    bg-white 
+                    text-emerald-800 
+                    hover:bg-emerald-600 
+                    hover:text-white 
+                    py-2 
+                    transition-colors 
+                    duration-300" type="submit">Login</button>
                 </form>
                 {message && <p className="text-red-500">{message}</p>}
             </div>

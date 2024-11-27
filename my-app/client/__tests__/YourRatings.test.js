1a9480b25c08a0e09fa4c6c1dbc05cab41360340
James <jamessheldona@gmail.com>
2024-11-26 23:07:45 -0500

    Your Ratings test

--- my-app/client/__tests__/YourRatings.test.js
+++ my-app/client/__tests__/YourRatings.test.js
@@ -3,14 +3,14 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { MemoryRouter, Route, Routes, } from 'react-router-dom';
 import Login from '../src/app/components/Login.jsx';
 import StudentDashboard from '../src/app/components/Student/StudentDashboard.jsx';
-import StudentManageClasses from '../src/app/components/Student/StudentManageClasses.jsx';
+import StudentReviewRatings from '../src/app/components/Student/StudentReviewRatings.jsx';
 import axios from 'axios';
 import '@testing-library/jest-dom/extend-expect';
 import StudentSidebar from '../src/app/components/_StudentSidebar.jsx';
 
 jest.mock('axios');
 
-test('navigates and clicks "My Classes" button on the sidebar', async () => {
+test('navigates and clicks "Your Ratings" button on the sidebar', async () => {
     // Mock login response
     axios.post.mockResolvedValueOnce({
         data: { message: 'Login successful', user: { id: '40123456' } },
@@ -37,21 +37,22 @@ test('navigates and clicks "My Classes" button on the sidebar', async () => {
     );
 
     // Scope to the sidebar
-  const sidebar = screen.getByTestId('sidebar');
-  sidebar.click();
-  const myClassesButton = screen.getByText(/My Classes/i);
-  
-  expect(myClassesButton).toBeInTheDocument();
+    const sidebar = screen.getByTestId('sidebar');
+    sidebar.click();
+    const myClassesButton = screen.getByText(/Your Ratings/i);
+    
+    expect(myClassesButton).toBeInTheDocument();
 
     // Simulate a click
     myClassesButton.click();
+
     render(
         <MemoryRouter>
-            <StudentManageClasses />
+            <StudentReviewRatings />
         </MemoryRouter>
     );
 
     // Verify the navigation
-    await waitFor(() => expect(screen.getByTestId("my-classes")).toBeInTheDocument());
+    await waitFor(() => expect(screen.getByTestId("your-ratings")).toBeInTheDocument());
 
 });
